d43ee406a18772810f5fd9d57bd118521167d8f0
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-01 13:44:25 +0600

    details

--- js/app.js
+++ js/app.js
@@ -22,19 +22,57 @@ const displayPhone = (phones) => {
                         <h3>Name : ${phone.phone_name}</h3>
                         <h5>Brand : ${phone.brand}</h5>
                         <div class="d-flex justify-content-end">
-                            <button class="btn btn-primary">Details</button>
+                            <button onclick="detailsPhone('${phone.slug}')" class="btn btn-primary">Details</button>
                         </div>
                     </div>
                 </div> 
         `
         phoneDivContainer.appendChild(div);
-
-
-        // console.log(phone);
-        // console.log(phone.image);
-        // console.log(phone.phone_name);
-        // console.log(phone.brand);
         // console.log(phone.slug);
     }
-
 }
+
+const detailsPhone = (phoneId) => {
+    url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetailsPhone(data.data))
+}
+
+const displayDetailsPhone = (phone) => {
+    console.log(phone)
+    console.log(phone.image)
+    console.log(phone.name)
+    // console.log(phone.releaseDate)
+    // console.log(phone.brand)
+    // console.log(phone.mainFeatures.chipSet)
+    // console.log(phone.mainFeatures.displaySize)
+    // console.log(phone.mainFeatures.storage)
+    // sensor    
+    console.log(phone.mainFeatures.sensors[0])
+    console.log(phone.mainFeatures.sensors[1])
+    console.log(displaySensor(phone.mainFeatures.sensors))
+    // others
+    // console.log(phone.others.Bluetooth)
+    // console.log(phone.others.GPS)
+    // console.log(phone.others.NFC)
+    // console.log(phone.others.Radio)
+    // console.log(phone.others.USB)
+    // console.log(phone.others.WLAN)
+    // console.log(phone)
+    // console.log(phone)
+}
+
+/*
+ onst displaySensor = (sensors) => {
+    console.log(sensors);
+    let sumSensor;
+    for (const sensor of sensors) {
+        // console.log(sensor)
+        sumSensor = sensor
+
+    }
+    return sumSensor;
+
+} 
+*/
